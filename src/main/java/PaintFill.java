6c8b51e99477047980aebd70c450fbd0769c29ba
Carlos Londoño <calondonop@gmail.com>
2020-12-05 11:36:59 -0500

    New approach and understanding from the challenge

--- src/main/java/PaintFill.java
+++ src/main/java/PaintFill.java
@@ -1,54 +1,14 @@
-import javafx.util.Pair;
-
-import java.lang.reflect.Array;
-import java.util.*;
-
 public class PaintFill {
     public static void main(String[] args) {
-
     }
-
-    public static Color[][] paintFill(Color[][] screen, int x, int y, Color newColor){
-        int rows = screen.length;
-        int columns = screen[0].length;
-        if(validatePointExist(rows, columns, x, y)){
-            //First we have a list with the positions we'll change the color, all points around the given point.
-            List<Pair<Integer,Integer>> pointToChangeColor = new ArrayList<>();
-            pointToChangeColor.add(new Pair<>(x-1, y-1));
-            pointToChangeColor.add(new Pair<>(x, y-1));
-            pointToChangeColor.add(new Pair<>(x+1, y-1));
-            pointToChangeColor.add(new Pair<>(x-1, y+1));
-            pointToChangeColor.add(new Pair<>(x, y+1));
-            pointToChangeColor.add(new Pair<>(x+1, y+1));
-            pointToChangeColor.add(new Pair<>(x-1, y));
-            pointToChangeColor.add(new Pair<>(x+1, y));
-
-            Color[][] finalScreen = screen;
-            pointToChangeColor.stream().forEach((pair) -> {
-                if(validatePointExist(rows, columns, pair.getKey(), pair.getValue())){
-                    changeColor(finalScreen, pair.getKey(), pair.getValue(), newColor);
-                }
-            });
-            screen = finalScreen;
+    public static void paintFill (Color [][] screen,Color oldColor, Color newColor,int x,int y)
+    {
+        if(x>=0 && y>=0 && x < screen.length && y<screen[0].length && screen[x][y].equals(oldColor)){
+            screen[x][y]=newColor;
+            paintFill(screen,oldColor,newColor, x-1, y);
+            paintFill(screen, oldColor, newColor,x+1, y);
+            paintFill(screen, oldColor, newColor, x, y+1);
+            paintFill(screen, oldColor, newColor, x, y-1);
         }
-        return screen;
-    }
-
-    public static boolean validatePointExist(int rows, int columns, int x, int y){
-        /* Taking as a reference the matrix length and positive positions.
-           Determine if the point exist inside the matrix*/
-        if (x>=0 && y>=0 && x<rows && y<columns) {
-            return true;
-        }else{
-            return false;
-        }
-    }
-
-    public static Color[][] changeColor(Color[][] screen, int x, int y, Color newColor){
-        //When the color in the specified position is different to the newColor, is changed
-        if (screen[x][y] != newColor) {
-            screen[x][y] = newColor;
-        }
-        return screen;
     }
 }
--- src/test/java/PaintFillTest.java
+++ src/test/java/PaintFillTest.java
@@ -1,4 +1,3 @@
-import org.junit.Assert;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -6,76 +5,44 @@ import static org.junit.Assert.*;
 public class PaintFillTest {
 
     Color white = Color.White;
-    Color orange = Color.Orange;
     Color pink = Color.Pink;
     Color blue = Color.Blue;
+    Color purple = Color.Purple;
 
     @Test
     public void paintFillTest(){
         //Arrange
         Color[][] screen = new Color[4][4];
-        screen[0] = new Color[]{white, white, white, white};
-        screen[1] = new Color[]{white, orange, white, white};
-        screen[2] = new Color[]{white, white, white, white};
-        screen[3] = new Color[]{white, white, white, white};
+
+        screen[0] = new Color[]{blue, blue, blue, blue};
+        screen[1] = new Color[]{blue, pink, pink, pink};
+        screen[2] = new Color[]{blue, blue, blue, blue};
+        screen[3] = new Color[]{blue, blue, blue, pink};
 
         //Expected Matrix case 1
         Color[][] expectedScreen1 = new Color[4][4];
-        expectedScreen1[0] = new Color[]{pink, pink, pink, white};
-        expectedScreen1[1] = new Color[]{pink, orange, pink, white};
-        expectedScreen1[2] = new Color[]{pink, pink, pink, white};
-        expectedScreen1[3] = new Color[]{white, white, white, white};
+        expectedScreen1[0] = new Color[]{blue, blue, blue, blue};
+        expectedScreen1[1] = new Color[]{blue, purple, purple, purple};
+        expectedScreen1[2] = new Color[]{blue, blue, blue, blue};
+        expectedScreen1[3] = new Color[]{blue, blue, blue, pink};
 
         //Expected Matrix case 2
         Color[][] expectedScreen2 = new Color[4][4];
-        expectedScreen2[0] = new Color[]{pink, blue, pink, white};
-        expectedScreen2[1] = new Color[]{blue, blue, pink, white};
-        expectedScreen2[2] = new Color[]{pink, pink, pink, white};
-        expectedScreen2[3] = new Color[]{white, white, white, white};
+        expectedScreen2[0] = new Color[]{white, white, white, white};
+        expectedScreen2[1] = new Color[]{white, purple, purple, purple};
+        expectedScreen2[2] = new Color[]{white, white, white, white};
+        expectedScreen2[3] = new Color[]{white, white, white, pink};
 
         //Act - In this case we are going to validate a given point centered in the matrix
-        Color[][] resultScreen1 = PaintFill.paintFill(screen, 1, 1, Color.Pink);
+        PaintFill.paintFill(screen, pink, purple, 1, 1);
 
         //Assert
-        assertArrayEquals(expectedScreen1, resultScreen1);
+        assertArrayEquals(expectedScreen1, screen);
 
         //Act - We are going to validate it work when the given point is in a extreme in the matrix
-        Color[][] resultScreen2 = PaintFill.paintFill(screen, 0, 0, Color.Blue);
+        PaintFill.paintFill(screen, blue, white, 3, 0);
 
         //Assert
-        assertArrayEquals(expectedScreen2, resultScreen2);
+        assertArrayEquals(expectedScreen2, screen);
     }
-
-    @Test
-    public void validatePointExist(){
-        //Act - Assert False
-        //Those position cannot exist in a 4x4 matrix
-        assertFalse(PaintFill.validatePointExist(4,4, -1,3));
-        assertFalse(PaintFill.validatePointExist(4,4, 6,6));
-        assertFalse(PaintFill.validatePointExist(4,4, 1,-2));
-
-        //Those position exist in a 4x4 matrix
-        assertTrue(PaintFill.validatePointExist(4,4, 3,3));
-        assertTrue(PaintFill.validatePointExist(4,4, 0,0));
-        assertTrue(PaintFill.validatePointExist(4,4, 2,2));
-    }
-
-    @Test
-    public void changeColorTest(){
-        //Arrange
-        Color[][] screen = new Color[2][2];
-        screen[0] = new Color[]{blue, blue};
-        screen[1] = new Color[]{blue, blue};
-
-        Color[][] expectedScreen = new Color[2][2];
-        expectedScreen[0] = new Color[]{orange, blue};
-        expectedScreen[1] = new Color[]{blue, blue};
-
-        //Act
-        Color[][] resultScreen = PaintFill.changeColor(screen, 0,0, orange);
-
-        //Assert
-        assertArrayEquals(expectedScreen, resultScreen);
-    }
-
 }
